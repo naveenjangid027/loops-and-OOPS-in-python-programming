d23d4a4a344ea551e8e8765eee84f2cad1543e6d
naveenjangid027 <147740082+naveenjangid027@users.noreply.github.com>
2024-10-28 19:53:23 +0530

    Update OOPSClass.py

--- OOPSClass.py
+++ OOPSClass.py
@@ -1,67 +1,67 @@
 # WAP tp print  the  class & object in python 
-#class Student():
-#   name = "naveen"
- #   Rollno = 34253
-  #  StudentID = 14993
+class Student():
+   name = "naveen"
+  Rollno = 34253
+    StudentID = 14993
 
-#s1 = Student()
-#print(s1.name)
-#print(s1.Rollno)
-#print(s1.StudentID)
+s1 = Student()
+print(s1.name)
+print(s1.Rollno)
+print(s1.StudentID)
 
 
 # WAP to class name is Car 
-#class Car():
-  #  brand = "mercedes"
-  # color ="blue"
- #   price = "45.24"
+class Car():
+    brand = "mercedes"
+   color ="blue"
+   price = "45.24"
 
-#car1 = Car()
-#print(car1.color)
-#print(car1.brand)
-#print(car1.price)
+car1 = Car()
+print(car1.color)
+print(car1.brand)
+print(car1.price)
 
-# Creating Class 
-#class Student:
-   #  def _init_(self):
- #     print(self)
-#      print("adding new student in Database...")
-#s1 = Student()
-#print(s1)
+# write a programme by using the constructor
+ Creating Class 
+class Student:
+     def _init_(self):
+ print(self)
+      print("adding new student in Database...")
+s1 = Student()
+print(s1)
 
 
 # WAP to the code 
-#class college():
- #def _init_(self,fullname):
-  #          self.name = fullname
- #           print("adding the new student Database")
-#s1 = college("kana")
-#print(s1.name)
-#s2 =college("naveen")
-#print(s2.name)
+class college():
+ def _init_(self,fullname):
+            self.name = fullname
+           print("adding the new student Database")
+1 = college("kana")
+print(s1.name)
+s2 =college("naveen")
+print(s2.name)
 
 
 # WAP to print
-##   college_name = "Vievkananda Institute of Technology "
-  #  name = "anoymus" 
- # class attribute 
-#def __init__(self,name):
-#class Student:
+college_name = "Vievkananda Institute of Technology "
+    name = "anoymus" 
+ class attribute 
+ef __init__(self,name):
+class Student:
  
- ##      self.name = name  
-   #     self.marks = marks 
-#
- ##      print("welcome student,", self.name)
-   #  
-    # def get_marks(self):
-     #             return self.marks
+      self.name = name  
+       self.marks = marks 
+
+      print("welcome student,", self.name)
+    
+     def get_marks(self)
+     return self.marks
       
+s1  = Student("karan",97)
+s1.welcome()
+print(s1.get_marks())
 
-
-#s1  = Student("karan",97)
-#s1.welcome()
-#print(s1.get_marks())
-
+# Write a programme 
 class Student:
     def __init__(self,name,marks):
         self.name = name 
